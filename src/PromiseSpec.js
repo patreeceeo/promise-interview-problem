3ae11aa2b422ac88e09c3e7bc7143fb1a2cdc919
Patrick <pcanfield@metromile.com>
2016-06-01 15:09:06 -0700

    Failing spec for basic resolve method

--- src/PromiseSpec.js
+++ src/PromiseSpec.js
@@ -4,6 +4,22 @@ describe("Promise", function () {
   "use strict";
 
   describe("#resolve", function () {
+    var fetchTweets, tweets;
+
+    beforeEach(function () {
+      fetchTweets = new Promise();
+      tweets = { fake: "data" };
+    });
+
+    it("calls all done callbacks with the resolved value", function (done) {
+      var spy = jasmine.createSpy("done");
+      fetchTweets.done(spy);
+      setTimeout(function () {
+        fetchTweets.resolve(tweets);
+        done();
+      });
+      expect(spy).toHaveBeenCalledWith(tweets);
+    });
   });
 });
 
