feb14e9fd946e1708f296788d627090c96f81246
Patrick <pcanfield@metromile.com>
2016-06-01 15:07:44 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+/* global module */
+module.exports = {
+  "rules": {
+    "no-trailing-spaces": [ 1 ],
+    "no-unused-expressions": [
+      1,
+      {
+        allowShortCircuit: true,
+        allowTernary: true
+      }
+    ],
+    "vars-on-top": [ 1 ],
+    "strict" : [
+      2,
+      "function"
+    ],
+    "indent": [
+      2,
+      2
+    ],
+    // "quotes": [
+    //   2,
+    //   "single"
+    // ],
+    "linebreak-style": [
+      2,
+      "unix"
+    ],
+    "semi": [
+      2,
+      "always"
+    ]
+  },
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "ecmaFeatures": {
+  }
+};
---
+++ src/PromiseSpec.js
@@ -0,0 +1,10 @@
+/* eslint-env jasmine */
+
+describe("Promise", function () {
+  "use strict";
+
+  describe("#resolve", function () {
+  });
+});
+
+
